b0645041fe60ad8fa53bfc07e9a8a440a125e2c0
omar9999024 <134130542+omar9999024@users.noreply.github.com>
2023-05-20 12:01:19 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,162 @@
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.add('active');
+}
+
+document.querySelector('#nav-close').onclick = () =>{
+    navbar.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.add('active');
+}
+
+document.querySelector('#close-search').onclick = () =>{
+    searchForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+};
+
+window.onload = () =>{
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+};
+
+
+var swiper = new Swiper(".home-slider", {
+    loop:true, 
+    grabCursor:true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
+
+var swiper = new Swiper(".product-slider", {
+    loop:true, 
+    grabCursor:true,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        640: {
+          slidesPerView: 2,
+        },
+        768: {
+          slidesPerView: 3,
+        },
+        1024: {
+          slidesPerView: 4,
+        },
+    },
+});
+
+var swiper = new Swiper(".review-slider", {
+    loop:true, 
+    grabCursor:true,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        640: {
+          slidesPerView: 2,
+        },
+        768: {
+          slidesPerView: 3,
+        },
+    },
+});
+
+var swiper = new Swiper(".blogs-slider", {
+    loop:true, 
+    grabCursor:true,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        991: {
+          slidesPerView: 3,
+        },
+    },
+});
+
+var swiper = new Swiper(".clients-slider", {
+    loop:true, 
+    grabCursor:true,
+    spaceBetween: 20,
+    breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        640: {
+          slidesPerView: 2,
+        },
+        768: {
+          slidesPerView: 3,
+        },
+        1024: {
+          slidesPerView: 4,
+        },
+    },
+});
+// Get the popup container and close button
+const popupContainer = document.getElementById('popup-container');
+const closePopup = document.getElementById('close-popup');
+
+// Show the popup when the page loads
+window.addEventListener('load', () => {
+    popupContainer.style.display = 'flex';
+});
+
+// Close the popup when the close button is clicked
+closePopup.addEventListener('click', () => {
+    popupContainer.style.display = 'none';
+});
+
+// Prevent the form from submitting and display a message instead
+const form = document.querySelector('form');
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    alert(`Thank you for signing up, ${name}! You will receive our newsletter at ${email} shortly.`);
+});
+// Add JavaScript code to listen for changes in the star rating
+document.querySelectorAll('#starRating input[type="radio"]').forEach(function (radio) {
+    radio.addEventListener('change', function () {
+        // Log the selected rating
+        console.log(this.value);
+    });
+});
